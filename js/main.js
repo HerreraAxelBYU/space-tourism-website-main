1138f9662fd916c04b22e99d74cab4eed2b31a6b
HerreraAxelBYU <her21075@byui.edu>
2022-08-20 11:29:30 -0300

    mobile menu

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const hamburguer = document.querySelector('#hamburguer_btn');
+const close = document.querySelector('#close_btn');
+const hidden_nav = document.querySelector('#hidden_menu');
+
+
+hamburguer.addEventListener('click', showMenu);
+close.addEventListener('click', hideMenu);
+
+
+
+function showMenu(){
+    hidden_nav.classList.remove('hidden');
+}
+
+function hideMenu(){
+    hidden_nav.classList.add('hidden');
+}
